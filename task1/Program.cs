5b193fb472f29533550e56a347d8a8361650b1a1
Katya <bozhkova-ekaterina@mail.ru>
2022-10-06 12:36:31 +0300

    добавили решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+int saturday = 6;
+int sunday = 7;
+if (day == saturday || day == sunday)
+{
+    Console.WriteLine("Выбранный день является выходным");
+}
+else
+{
+    Console.WriteLine("Выбранный день не является выходным");
+}
